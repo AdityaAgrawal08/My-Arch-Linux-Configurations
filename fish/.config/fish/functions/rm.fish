3ce56651ea6486cee42a103a7c302cfd8ec97fea
Aditya Agrawal <adityaagrawal1908@gmail.com>
2026-04-18 13:47:47 +0530

    final: CAS trash system with directory manifests, atomic restore, scan, pinned safety

--- fish/.config/fish/functions/rm.fish
+++ fish/.config/fish/functions/rm.fish
@@ -1,29 +1,3 @@
 function rm
-    set files
-    set dirs
-
-    for arg in $argv
-        if test -d $arg
-            set dirs $dirs $arg
-        else
-            set files $files $arg
-        end
-    end
-
-    # Recursive delete → bypass trash (explicit + consistent)
-    if contains -- -r $argv; or contains -- -rf $argv; or contains -- -fr $argv
-        echo "Recursive delete bypasses trash"
-        command rm $argv
-        return
-    end
-
-    # Files → trash
-    if test (count $files) -gt 0
-        /usr/bin/env bash ~/.local/bin/safe-rm $files
-    end
-
-    # Directories → normal rm
-    if test (count $dirs) -gt 0
-        command rm $dirs
-    end
+    /usr/bin/env bash ~/.local/bin/safe-rm $argv
 end
