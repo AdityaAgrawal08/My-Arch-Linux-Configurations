8c0976dd2bfe84eb99720ed0572f208e8f0d314c
Aditya Agrawal <adityaagrawal1908@gmail.com>
2026-05-21 03:15:11 +0530

    Added Sleep

--- hypr/.config/hypr/scripts/power-menu.sh
+++ hypr/.config/hypr/scripts/power-menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-options="  Lock\n  Restart\n⏻  Shutdown"
-chosen=$(echo -e "$options" | wofi --dmenu --prompt "Power Menu")
+options="  Lock\n  Sleep\n  Restart\n⏻  Shutdown"
+chosen=$(echo -e "$options" | wofi --dmenu --prompt "Power Menu" --no-sort --matching=none)
 echo "Chosen: '$chosen'" >/tmp/power-menu.log
 case "$chosen" in
     *Lock*)
@@ -13,11 +13,23 @@ case "$chosen" in
         wait
         mpv --no-terminal /home/aditya/Downloads/open.wav
         ;;
+
+    *Sleep*)
+        pkill wofi
+        mpv --no-terminal /home/aditya/Downloads/close.wav &
+        MPV_PID=$!
+        hyprlock &
+        sleep 0.5
+        systemctl suspend
+        kill $MPV_PID 2>/dev/null
+        ;;
+
     *Restart*)
         pkill wofi
         sleep 0.1
         systemctl reboot
         ;;
+
     *Shutdown*)
         pkill wofi
         sleep 0.1
