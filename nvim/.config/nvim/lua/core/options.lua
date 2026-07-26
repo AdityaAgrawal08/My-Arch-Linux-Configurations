58020faeed44852ba5f35da87706e90b746fb66a
Aditya Agrawal <adityaagrawal1908@gmail.com>
2026-07-26 13:54:59 +0530

    fix(telescope): switch to master branch to fix ft_to_lang crash in Neovim 0.11

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,12 +1,3 @@
--- Compatibility shim: Neovim 0.11 removed ft_to_lang (Telescope still uses it in previewers)
-if not vim.treesitter.language.ft_to_lang then
-  vim.treesitter.language.ft_to_lang = setmetatable({}, {
-    __index = function(_, key)
-      return vim.treesitter.language.get_lang(key) or key
-    end
-  })
-end
-
 -- Leader keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
--- nvim/.config/nvim/lua/plugins/files.lua
+++ nvim/.config/nvim/lua/plugins/files.lua
@@ -60,7 +60,6 @@ return {
   -- Telescope
   {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
     cmd = "Telescope",
     dependencies = {
       "nvim-lua/plenary.nvim",
